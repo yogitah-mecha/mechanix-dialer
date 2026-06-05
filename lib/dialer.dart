850d4789afca32ba4210d21821f666e61ba8f865
yogitah-mecha <yogitah@mechasystems.com>
2026-06-05 16:37:53 +0530

    object box export

--- lib/dialer.dart
+++ lib/dialer.dart
@@ -3,3 +3,4 @@ export 'features/contacts/data/models/email.dart';
 export 'features/contacts/data/models/phone_numbers.dart';
 export 'features/contacts/data/repositories/contacts_repository_impl.dart';
 export 'features/contacts/data/repositories/contact_store.dart';
+export 'objectbox.g.dart';
